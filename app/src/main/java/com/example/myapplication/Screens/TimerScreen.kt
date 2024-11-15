bab6da85be187211fda32b884c108c1e22390ee9
JasonM220 <1957180@johnabbottcollege.net>
2024-11-15 11:05:25 -0500

    updated the timer screen to add circular bar and start/stop button

--- app/src/main/java/com/example/myapplication/Screens/TimerScreen.kt
+++ app/src/main/java/com/example/myapplication/Screens/TimerScreen.kt
@@ -1,5 +1,6 @@
 package com.example.myapplication.Screens
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
@@ -11,14 +12,16 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.sp
 import kotlinx.coroutines.delay
 
+@SuppressLint("DefaultLocale")
 @Composable
 fun TimerWithProgressBar() {
     var timeElapsed by remember { mutableStateOf(60) }
     val totalTime = 60
     val progress = (totalTime - timeElapsed) / totalTime.toFloat()
+    var isRunning by remember { mutableStateOf(false) }
 
-    LaunchedEffect(timeElapsed) {
-        if (timeElapsed > 0) {
+    LaunchedEffect(isRunning) {
+        while (isRunning && timeElapsed > 0) {
             delay(1000)
             timeElapsed -= 1
         }
@@ -29,21 +32,20 @@ fun TimerWithProgressBar() {
             .fillMaxSize()
             .padding(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+        verticalArrangement = Arrangement.SpaceAround
     ) {
+
+        CircularProgressIndicator(
+            progress = { progress },
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(10.dp),
+            color = MaterialTheme.colorScheme.primary,
+        )
+
         Text(
             text = String.format("%02d:%02d", timeElapsed / 60, timeElapsed % 60),
             style = TextStyle(fontSize = 48.sp, color = Color.Black)
-        )
-
-        Spacer(modifier = Modifier.height(32.dp))
-
-        LinearProgressIndicator(
-            progress = {
-                progress
-            },
-            modifier = Modifier.fillMaxWidth().height(10.dp),
-            color = MaterialTheme.colorScheme.primary,
         )
 
         if (timeElapsed == 0) {
@@ -51,6 +53,19 @@ fun TimerWithProgressBar() {
             Text(
                 text = "Time's Up!",
                 style = TextStyle(fontSize = 24.sp, color = Color.Red)
+            )
+        }
+
+        Spacer(modifier = Modifier.height(32.dp))
+
+        Button(
+            onClick = {
+                isRunning = !isRunning
+            }
+        ) {
+            Text(
+                text = if (isRunning) "Stop Timer" else "Start Timer",
+                style = TextStyle(fontSize = 18.sp)
             )
         }
     }
