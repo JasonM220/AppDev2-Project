13b74d6926eda28940286a9bf9a5ff1573fa9070
Amaan6868 <2258069@johnabbottcollege.net>
2024-11-25 11:41:13 -0500

    Added Register Screen

--- app/src/main/java/com/example/myapplication/Screens/RegisterScreen.kt
+++ app/src/main/java/com/example/myapplication/Screens/RegisterScreen.kt
@@ -1,4 +1,4 @@
-package com.example.myapplication.Screens
+﻿package com.example.myapplication.Screens
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
