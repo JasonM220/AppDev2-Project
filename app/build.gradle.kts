f71c3f931ea7b5f586381ee4fb5d1af2bf448f96
JasonM220 <1957180@johnabbottcollege.net>
2024-11-29 10:52:10 -0500

    added sending to the database the meditation data

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,6 +66,7 @@ dependencies {
     implementation(libs.firebase.auth.ktx)
     implementation(libs.firebase.auth)
     implementation(libs.androidx.navigation.compose)
+    implementation(libs.firebase.firestore.ktx)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
